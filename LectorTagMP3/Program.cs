3a85133578ec749b5f7fcd7e901ea2fadd7951d9
davidcba01 <davoocordoba2001@gmail.com>
2025-06-10 16:52:24 -0300

    Agrego código de LectorTagMP3 FALTA TERMINAR

---
+++ LectorTagMP3/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Cancion
+{
+    public string Header { get; set; }
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public string Album { get; set; }
+    public uint anio { get; set; }
+    public string Comentario { get; set; }
+    public string Genero { get; set; }
+}
+
+//------------
+using System;
+using System.Collections.Generic;
+using System.IO;
+using TagLib;
+
+class Program
+{
+    static void Main(){
+        List<Cancion> canciones = new List<Cancion>();
+
+        Console.Write("Ingrese la ruta de una carpeta con archivos MP3: ");
+        string path = Console.ReadLine();
+
+        if (!Directory.Exists(path))
+        {
+            Console.WriteLine("Carpeta no encontrada.");
+            return;
+        }
+
+        string[] archivos = Directory.GetFiles(path, "*.mp3");
+
+        foreach (string archivo in archivos)
+        {
+            try
+            {
+                var mp3 = TagLib.File.Create(archivo);
+                Cancion c = new Cancion
+                //=================TERMINARRRRRRRRR
+            }
+            catch (System.Exception)
+            {
+                
+                throw;
+            }
+        }
+    }
+}
